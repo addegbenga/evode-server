d72a1aec56d29098518ca07ca6844cdf510b4eeb
jsaura27 <lenxltd@gmail.com>
2021-03-28 15:55:35 +0100

    update

--- BackEnd/routes/users.js
+++ BackEnd/routes/users.js
@@ -1,129 +1,131 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const passport = require('passport');
-const bcrypt = require('bcryptjs');
-const User = require('../models/User');
-const { ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
+const passport = require("passport");
+const bcrypt = require("bcryptjs");
+const User = require("../models/User");
+const {
+  loginValidations,
+  registerValidations,
+} = require("../config/validation");
+const { ensureAuthenticated, forwardAuthenticated } = require("../config/auth");
 
-
-//Get Requests
-  //Login
-router.get('/login',
-  (req, res) => res.render('login')
+//Login
+router.get("/login", (req, res) =>
+  res.render("login", { message: req.flash("loginMessage") })
 );
-  //Register
-router.get('/register',
-  (req, res) => res.render('register')
+//Register
+router.get("/register", (req, res) =>
+  res.render("register", { message: req.flash("signupMessage") })
 );
 
 //2 factor authenticator
 
-router.get('/setup-2fa',
-  (req, res) => res.render('setup-2fa')
-);
+router.get("/setup-2fa", (req, res) => res.render("setup-2fa"));
 
-//Post Requests
-router.post('/register',
-  (req, res) =>{
-    const{name,email,password, password2} = req.body;
-    const regularExpression = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]/;
-    let errors = [];
-
-    //Check fields are all filled
-    if(!name || !email || !password || !password2){
-      errors.push({msg: 'Please fill all the fields'});
+//Register with passport
+router.post("/register", function (req, res, next) {
+  passport.authenticate("register", function (err, user, info) {
+    if (err) {
+      req.flash("signupMessage", err);
     }
-
-    //Check both passwords match
-    if(password !== password2){
-      errors.push({msg: 'Passwords do not match'});
+    const { error } = registerValidations(req.body);
+    if (error) {
+      req.flash("signupMessage", error.details[0].message);
+      return res.redirect("/users/register");
+      // return res.status(400).json(error.details[0].message);
     }
-
-    //Check password length
-    if(password.length < 8 ){
-      errors.push({msg: "Password has to be at least 8 characters long "});
+    if (info) {
+      // res.status(401);
+      req.flash("signupMessage", info.message);
+      return res.redirect("/users/register");
+      // res.status(401).json(info.message);
     }
-    //Check password has a number or special character
-    if(!regularExpression.test(password)){
-      errors.push({msg: "Password has to include a number and special character"});
+    if (user) {
+      req.flash("loginMessage", "User created succesfully you can now login"),
+        console.log(user);
+      return res.redirect("/users/login");
     }
-
-    if(errors.length > 0){
-      res.render('register', {
-        errors,
-        name,
-        email,
-        password,
-        password2
-      });
-    }else{
-      //Validation has been succesful
-      User.findOne({email: email})
-        .then(user => {
-          if(user) {
-            //User exist
-            errors.push({msg: 'Email is already registered'})
-            res.render('register', {
-              errors,
-              name,
-              email,
-              password,
-              password2
-            });
-          }else{
-            const newUser = new User({
-              name,
-              email,
-              password
-            });
-
-            //Hash password
-            bcrypt.genSalt(10, (err, salt) =>
-              bcrypt.hash(newUser.password, salt, (err, hash) =>
-                {
-                  if(err) throw err;
-                  //Here we encrypt the password
-                  newUser.password = hash;
-
-                  //Save the user in the database
-                  newUser.save()
-                    .then(user => {
-                      req.flash('success_msg', 'You are now registered and can log in');
-                      res.redirect('login');
-                    })
-                    .catch(err => console.log(err));
-                })
-              );
-          }
-        })
-      ;
-    }
-
-  }
-);
+  })(req, res, next);
+});
 
 //Login with Passport
 
-router.post('/login', (req, res, next) => {
-  passport.authenticate('local', {
-    successRedirect: '/dashboard',
-    failureRedirect: '/users/login',
-    failureFlash: true
+router.post("/login", function (req, res, next) {
+  passport.authenticate("login", function (err, user, info) {
+    console.log(req.body);
+    if (err) {
+      return res.json(err);
+    }
+    const { error } = loginValidations(req.body);
+    if (error) {
+      req.flash("loginMessage", error.details[0].message);
+      // res.json(error.details[0].message);
+      return res.redirect("/users/login");
+    }
+    if (info) {
+      // res.status(401);
+      req.flash("loginMessage", info.message);
+      // res.json(info);
+      return res.redirect("/users/login");
+    }
+    req.logIn(user, function (err) {
+      if (err) {
+        return next(err);
+      }
+      // res.json(user);
+      return res.redirect("../dashboard");
+    });
   })(req, res, next);
 });
 
 //Delete user
 
-router.get('/delete', ensureAuthenticated, (req, res) =>
-  res.render('delete', {
-    user: req.user
+router.get("/delete", ensureAuthenticated, (req, res) =>
+  res.render("delete", {
+    user: req.user,
   })
 );
+
+//Change password page
+router.get("/passwordChange", ensureAuthenticated, (req, res)=>
+  res.render("passwordChange",{
+    user:req.user,
+  })
+);
+
+//Change password with passport
+
+router.post("/passwordChange",  (req, res, next) =>{
+  passport.authenticate("password change", function (err, user, info) {
+    if (err) {
+      req.flash("signupMessage", err);
+    }
+    const { error } = registerValidations(req.body);
+    if (error) {
+      req.flash("signupMessage", error.details[0].message);
+      return res.redirect("/users/passwordChange");
+      // return res.status(400).json(error.details[0].message);
+    }
+    if (info) {
+      // res.status(401);
+      req.flash("signupMessage", info.message);
+      return res.redirect("/users/passwordChange");
+      // res.status(401).json(info.message);
+    }
+    if (user) {
+      req.flash("passwordMessage", "User password changed succesfully"),
+        console.log(user);
+      return res.redirect("/dashboard");
+    }
+  })(req, res, next);
+});
+
+
 //Logout
 
-router.get('/logout', (req, res) =>{
+router.get("/logout", (req, res) => {
   req.logout();
-  req.flash('success_msg', 'You are logged out');
-  res.redirect('/users/login');
+  req.flash("success_msg", "You are logged out");
+  res.redirect("/users/login");
 });
 module.exports = router;
