46bb06f622b6d41a71dd7c7eee526ba0a9f744af
jsaura27 <lenxltd@gmail.com>
2021-03-28 15:54:59 +0100

    password change

--- BackEnd/models/User.js
+++ BackEnd/models/User.js
@@ -1,23 +1,35 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 const UserSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true
+    required: true,
   },
   email: {
     type: String,
-    required: true
+    // required: true
   },
   password: {
     type: String,
-    required: true
+    required: true,
   },
   date: {
     type: Date,
-    default: Date.now
-  }
+    default: Date.now,
+  },
 });
 
-const User = mongoose.model('User', UserSchema);
+//Encrypt password using bcrypt
+UserSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+//Password matcher
+UserSchema.methods.matchPassword = async function (enteredPass) {
+  return await bcrypt.compare(enteredPass, this.password);
+};
+
+const User = mongoose.model("User", UserSchema);
 
 module.exports = User;
