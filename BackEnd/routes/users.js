dee18623af677fdcee52f41734b00b4dbeaf0658
jsaura27 <lenxltd@gmail.com>
2021-03-31 22:55:33 +0100

    Delete user update

--- BackEnd/routes/users.js
+++ BackEnd/routes/users.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const passport = require("passport");
-const bcrypt = require("bcryptjs");
 const User = require("../models/User");
 const {
   loginValidations,
   registerValidations,
+  changePasswordValidation,
 } = require("../config/validation");
 const { ensureAuthenticated, forwardAuthenticated } = require("../config/auth");
 
@@ -17,6 +17,7 @@ router.get("/login", (req, res) =>
 router.get("/register", (req, res) =>
   res.render("register", { message: req.flash("signupMessage") })
 );
+
 
 //2 factor authenticator
 
@@ -31,19 +32,19 @@ router.post("/register", function (req, res, next) {
     const { error } = registerValidations(req.body);
     if (error) {
       req.flash("signupMessage", error.details[0].message);
-      return res.redirect("/users/register");
-      // return res.status(400).json(error.details[0].message);
+      // return res.redirect("/users/register");
+      return res.status(400).json(error.details[0].message);
     }
     if (info) {
       // res.status(401);
       req.flash("signupMessage", info.message);
-      return res.redirect("/users/register");
-      // res.status(401).json(info.message);
+      // return res.redirect("/users/register");
+      return res.status(401).json(info.message);
     }
     if (user) {
       req.flash("loginMessage", "User created succesfully you can now login"),
         console.log(user);
-      return res.redirect("/users/login");
+      // return res.redirect("/users/login");
     }
   })(req, res, next);
 });
@@ -59,65 +60,115 @@ router.post("/login", function (req, res, next) {
     const { error } = loginValidations(req.body);
     if (error) {
       req.flash("loginMessage", error.details[0].message);
-      // res.json(error.details[0].message);
+      // return res.json(error.details[0].message);
       return res.redirect("/users/login");
     }
     if (info) {
       // res.status(401);
       req.flash("loginMessage", info.message);
-      // res.json(info);
+      // return res.json(info);
       return res.redirect("/users/login");
     }
     req.logIn(user, function (err) {
       if (err) {
         return next(err);
       }
-      // res.json(user);
+      // return res.json(user);
       return res.redirect("../dashboard");
     });
   })(req, res, next);
 });
+
+//Change Password page
+router.get("/passwordChange", ensureAuthenticated, (req, res) =>
+  res.render("passwordChange", {
+    user: req.user,
+    message: req.flash("forgotpassMessage"),
+  })
+);
+
+//Change password
+
+router.put("/passwordChange", ensureAuthenticated, async (req, res) => {
+  const id = req.user._id;
+  try {
+    const user = await User.findById({ _id: id });
+    if (!user) {
+      req.flash("forgotpassMessage", "user not authorized");
+      return res.status(403).json({ msg: "user not authorized" });
+    }
+    const newDetails = {
+      newPassword: req.body.newPassword,
+      currentPassword: req.body.currentPassword,
+    };
+    const { error } = changePasswordValidation(newDetails);
+    if (error) {
+      req.flash("forgotpassMessage", error.details[0].message);
+      return res.redirect("/users/passwordChange");
+      // return res.json(error.details[0].message);
+    }
+    const validate = await user.matchPassword(newDetails.currentPassword);
+    console.log(validate);
+    if (!validate) {
+      req.flash("forgotpassMessage", "password does not match record");
+      return res.redirect("/users/passwordChange");
+      // return res.json({ msg: "password does not match record" });
+    }
+    user.password = req.body.newPassword;
+    const newUser = await user.save();
+    req.flash("dashboardMessage", "password changed successfully");
+    return res.redirect("../dashboard");
+    // return res.json(newUser);
+  } catch (error) {
+    res.status(500).json({ msg: "server error" });
+    console.log(error);
+  }
+});
+
 
 //Delete user
 
 router.get("/delete", ensureAuthenticated, (req, res) =>
   res.render("delete", {
     user: req.user,
+    message: req.flash("forgotpassMessage"),
   })
 );
 
-//Change password page
-router.get("/passwordChange", ensureAuthenticated, (req, res)=>
-  res.render("passwordChange",{
-    user:req.user,
-  })
-);
 
-//Change password with passport
+//Delete user function
+router.put("/deleteUser", ensureAuthenticated, async (req, res) => {
+  const id = req.user._id;
+  try {
+    const user = await User.findById({ _id: id });
+    if (!user) {
+      req.flash("forgotpassMessage", "user not authorized");
+      return res.status(403).json({ msg: "user not authorized" });
+    }
+    const newDetails = {
+      email: req.body.email,
+      password: req.body.password,
+    };
 
-router.post("/passwordChange",  (req, res, next) =>{
-  passport.authenticate("password change", function (err, user, info) {
-    if (err) {
-      req.flash("signupMessage", err);
+    const validate = await user.matchPassword(newDetails.password);
+    console.log(validate);
+    if (!validate) {
+      req.flash("forgotpassMessage", "password does not match record");
+      return res.redirect("/users/delete");
+      // return res.json({ msg: "password does not match record" });
     }
-    const { error } = registerValidations(req.body);
-    if (error) {
-      req.flash("signupMessage", error.details[0].message);
-      return res.redirect("/users/passwordChange");
-      // return res.status(400).json(error.details[0].message);
-    }
-    if (info) {
-      // res.status(401);
-      req.flash("signupMessage", info.message);
-      return res.redirect("/users/passwordChange");
-      // res.status(401).json(info.message);
-    }
-    if (user) {
-      req.flash("passwordMessage", "User password changed succesfully"),
-        console.log(user);
-      return res.redirect("/dashboard");
-    }
-  })(req, res, next);
+    user.remove({_id: id},
+      function(err){
+          if(err) res.json(err);
+          else  req.flash("welcomeMessage", "user deleted successfully");
+          return res.redirect("/users/login");
+        });
+
+    // return res.json(newUser);
+  } catch (error) {
+    res.status(500).json({ msg: "server error" });
+    console.log(error);
+  }
 });
 
 
