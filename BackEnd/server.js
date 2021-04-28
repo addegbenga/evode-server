449f984713e29f7ef30f9953b444ab68d23f1074
addegbenga <addegbenga@gmail.com>
2021-04-28 07:54:03 +0100

    setup

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -29,8 +29,8 @@ app.use(cors("*"));
 app.use(morgan("tiny"));
 
 //setup api
-app.use("/api/auth", require("./routes2/auth"));
-app.use("/api/2fa", require("./routes2/2fa"));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/2fa", require("./routes/2fa"));
 
 const port = process.env.PORT || 5000; //port setting
 app.listen(port, () => console.log("App listening on port " + port));
